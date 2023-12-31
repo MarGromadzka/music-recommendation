0d8b2283d34d4ab0f33f365f5d23a298e912d27a
MarGromadzka <69045227+MarGromadzka@users.noreply.github.com>
2023-12-31 16:01:54 +0100

    added w2v to recommendation system

---
+++ src/features/Word2VecReader.py
@@ -0,0 +1,23 @@
+from gensim.models import KeyedVectors
+import pandas as pd
+
+
+class Word2VecReader:
+
+    def __init__(self, user_id):
+        self.wv = KeyedVectors.load("models/wv/word2vec.wordvectors", mmap='r')
+        self.user_song = pd.read_csv('data/processed/user_songs.csv')
+        self.user_id = user_id
+
+    def _get_similar_users(self):
+        try:
+            return [user[0] for user in self.wv.most_similar(positive=[self.user_id])[0:3]]
+        except KeyError:
+            return []
+    def get_songs(self):
+        similar_users = self._get_similar_users()
+        similar_songs = []
+        for user in similar_users:
+            similar_songs += self.user_song[self.user_song["cookieid"] == user].sort_values(["count"], ascending=False).head(3)[
+                "link_id"].tolist()
+        return similar_songs
